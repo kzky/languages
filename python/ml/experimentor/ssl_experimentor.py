b2519c76d2334c7ba34758048b66ac18c19a5fb5
kzky <rkzfilter@gmail.com>
2015-03-08 21:16:12 +0900

    test comp.

--- python/ml/experimentor/ssl_experimentor.py
+++ python/ml/experimentor/ssl_experimentor.py
@@ -8,15 +8,15 @@ import os
 from experimentor import Experimentor
 from collections import defaultdict
 from cloud.serialization import cloudpickle
-from ml.ssl import HPFSSL
-from ml.ssl import LapRLS
-from ml.ssl import SVM
+from ml.ssl.hpfssl import HPFSSLClassifier
+from ml.ssl.laprls import LapRLSClassifier
+from ml.ssl.svm import LSVMClassifier
 
 LABELED_DATASET_SUFFIX = "l"
 UNLABELED_DATASET_SUFFIX = "u"
 TEST_DATASET_SUFFIX = "t"
 
-SAMPLED_DATASET_NUMBER = 40
+SAMPLED_DATASETS_NUMBER = 40
 
 class SSLRateDatesetEvaluator(Experimentor):
     """
@@ -36,6 +36,7 @@ class SSLRateDatesetEvaluator(Experimentor):
 
         # base paths
         self.base_paths = base_paths
+        self.base_paths.sort()
         
         # output path
         self.output_path = output_path
@@ -61,6 +62,7 @@ class SSLRateDatesetEvaluator(Experimentor):
 
         for base_path in self.base_paths:
             dataset_paths = glob.glob("%s/*" % base_path)
+            dataset_paths.sort()
             for dataset_path in dataset_paths:  # for each dataset
                 self._run_with(dataset_path)
                 pass
@@ -77,7 +79,7 @@ class SSLRateDatesetEvaluator(Experimentor):
 
         # check for the num. of sampled datasets
         sampled_datasets_number = len(glob.glob("%s/*" % dataset_path))
-        if sampled_datasets_number != self.SAMPLED_DATASETS_NUMBER:
+        if sampled_datasets_number != SAMPLED_DATASETS_NUMBER:
             self.logger.info("%s is skipped due to shortage")
             return
 
@@ -102,47 +104,56 @@ class SSLRateDatesetEvaluator(Experimentor):
             dataset_path,
             index,
             LABELED_DATASET_SUFFIX)
-        ldata = np.loadtxt(lpath, delimiter=",")
+        ldata = np.loadtxt(lpath, delimiter=" ")
         y_l = ldata[:, 0]
         X_l = ldata[:, 1:]
+        l = X_l.shape[0]
+        X_l = np.hstack((X_l, np.reshape(np.ones(l), (l, 1))))
 
         # unlabeled dataset
         upath = "%s/%d_%s.csv" % (
             dataset_path,
             index,
             UNLABELED_DATASET_SUFFIX)
-        udata = np.loadtxt(upath, delimiter=",")
+        udata = np.loadtxt(upath, delimiter=" ")
         X_u = udata[:, 1:]
+        u = X_u.shape[0]
+        X_u = np.hstack((X_u, np.reshape(np.ones(u), (u, 1))))
         
         # validation dataset
         vpath = "%s/%d_%s.csv" % (
             dataset_path,
             index,
             UNLABELED_DATASET_SUFFIX)
-        vdata = np.loadtxt(vpath, delimiter=",")
+        vdata = np.loadtxt(vpath, delimiter=" ")
         X_v = vdata[:, 1:]
         y_v = vdata[:, 0]
+        v = X_v.shape[0]
+        X_v = np.hstack((X_v, np.reshape(np.ones(v), (v, 1))))
 
         # test dataset
         tpath = "%s/%d_%s.csv" % (
             dataset_path,
             index,
             TEST_DATASET_SUFFIX)
-        tdata = np.loadtxt(tpath, delimiter=",")
+        tdata = np.loadtxt(tpath, delimiter=" ")
         y_t = tdata[:, 0]
         X_t = tdata[:, 1:]
+        t = X_t.shape[0]
+        X_t = np.hstack((X_t, np.reshape(np.ones(t), (t, 1))))
 
         # keys of results map
         dataset_name = dataset_path.split("/")[-1]
-        rates = dataset_path.split("/")[-1].split("_")
+        rates = dataset_path.split("/")[-2].split("_")
         trate = rates[-1]
         vrate = rates[-2]
         urate = rates[-3]
         lrate = rates[-4]
         rate_pair = "%s_%s_%s_%s" % (lrate, urate, vrate, trate)
 
-        for classifier_name in self.classifiers:
-            param_grid = self.classifiers_info["classifier_name"]["param_grid"]
+        for classifier_name in self.classifiers_info:
+            param_grid = self.classifiers_info[classifier_name]["param_grid"]
+            classifier_ = self.classifiers_info[classifier_name]["classifier"]
             try:
                 classifier = classifier_.validate_in_ssl(X_l, y_l, X_u, X_v, y_v,
                                                          param_grid=param_grid)
@@ -167,7 +178,7 @@ class SSLRateDatesetEvaluator(Experimentor):
         - `dataset_path`:
         """
         indices = set()
-        for path in glob.glob(dataset_path):
+        for path in glob.glob("%s/*" % dataset_path):
             i = path.split("/")[-1].split("_")[0]
             indices.add(int(i))
 
@@ -195,23 +206,23 @@ class SSLRateDatesetEvaluator(Experimentor):
 def main():
 
     # arguemnts for ssl
-    base_paths = ["/home/kzk/datasets/uci_csv_ssl_1_50_1_48"]
-    output_path = "/home/kzk/datasets/uci_csv_ssl_1_50_1_48/experiment/final_paper_2015/test/test001.pkl"
+    base_paths = ["/home/kzk/datasets/uci_csv_ssl_1_50_1_48_subset"]
+    output_path = "/home/kzk/experiment/final_paper_2015/test/test001.pkl"
     classifiers_info = {
         "hpfssl": {
-            "classifier": HPFSSL(),
-            "param_grid": [{"max_itr": 50, "threshold": 1e-4, "learning": "batch"}],
+            "classifier": HPFSSLClassifier(),
+            "param_grid": [{"max_itr": 50, "threshold": 1e-4, "learn_type": "batch"}],
         },
         
         "svm": {
-            "classifier": SVM(),
+            "classifier": LSVMClassifier(),
             "param_grid": [
                 {"C": 1e-3}, {"C": 1e-2}, {"C": 1e-1}, {"C": 1},
                 {"C": 1e1}, {"C": 1e2}, {"C": 1e3}],
         },
 
         "laprls": {
-            "classifier": LapRLS(),
+            "classifier": LapRLSClassifier(),
             "param_grid": [
                 {"lam": 1e-3, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
                 {"lam": 1e-3, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
@@ -221,59 +232,58 @@ def main():
                 {"lam": 1e-3, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
                 {"lam": 1e-3, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
 
-#                {"lam": 1e-2, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-2, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-2, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-2, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-2, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-2, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-2, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
-# 
-#                {"lam": 1e-1, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-1, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-1, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-1, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-1, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-1, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e-1, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
-# 
-#                {"lam": 1e0, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e0, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e0, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e0, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e0, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e0, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e0, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
-# 
-#                {"lam": 1e1, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e1, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e1, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e1, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e1, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e1, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e1, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
-# 
-#                {"lam": 1e2, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e2, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e2, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e2, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e2, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e2, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e2, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
-# 
-#                {"lam": 1e3, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e3, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e3, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e3, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e3, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e3, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
-#                {"lam": 1e3, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-2, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-2, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-2, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-2, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-2, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-2, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-2, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
+ 
+                {"lam": 1e-1, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-1, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-1, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-1, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-1, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-1, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e-1, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
+ 
+                {"lam": 1e0, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e0, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e0, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e0, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e0, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e0, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e0, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
+ 
+                {"lam": 1e1, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e1, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e1, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e1, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e1, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e1, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e1, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
+ 
+                {"lam": 1e2, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e2, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e2, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e2, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e2, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e2, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e2, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
+ 
+                {"lam": 1e3, "gamma_s": 1e-3, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e3, "gamma_s": 1e-2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e3, "gamma_s": 1e-1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e3, "gamma_s": 1e0, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e3, "gamma_s": 1e1, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e3, "gamma_s": 1e2, "normalized": True, "kernel": "rbf"},
+                {"lam": 1e3, "gamma_s": 1e3, "normalized": True, "kernel": "rbf"},
                 
             ],
         },
         
     }
-    
 
     experimentor = SSLRateDatesetEvaluator(
         base_paths,
