62153ed52091434e0a8cffcbe50a3605d9504352
kzky <rkzfilter@gmail.com>
2015-06-14 22:30:05 +0900

    add logger

--- python/log/logger.py
+++ python/log/logger.py
@@ -1,7 +1,42 @@
 #!/usr/lib/python
 
 import logging
+import sys
 
-logging.basicConfig(level=logging.DEBUG)
-logging.info("info")
-logging.debug("debug")
+from logging import StreamHandler
+from logging import Formatter
+
+class LoggingTest(object):
+    """
+    """
+    
+    
+    FORMAT = '%(asctime)s::%(levelname)s::%(name)s::%(funcName)s::%(message)s'
+    logging.basicConfig(
+        format=FORMAT,
+        level=logging.DEBUG)
+
+    logger = logging.getLogger("LoggingTest")
+    
+    def __init__(self, ):
+        """
+        """
+        
+    def add(self, a, b):
+        """
+        
+        Arguments:
+        - `a`:
+        - `b`:
+        """
+        self.logger.info("add start")
+        
+        return a + b
+        
+def main():
+    c = LoggingTest()
+    c.add(5, 5)
+    pass
+
+if __name__ == "__main__":
+    main()
