37c2e0f6f4a5c02d7a840c7cf77e7c74b2464c13
kzky <rkzfilter@gmail.com>
2015-07-26 03:21:27 +0900

    add flask-login.py sample.

---
+++ python/flask/flask-login.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+from flask import Flask, request, jsonify, g
+from flask.ext.login import LoginManager, UserMixin, login_required
+from flask.ext.mongoengine import MongoEngine
+
+# Init application
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret_key"
+
+# Init db
+db = MongoEngine()
+db_name = "flask-login-sample"
+app.config['MONGODB_SETTINGS'] = {
+    'db': db_name,
+    # 'username':'webapp',
+    # 'password':'pwd123'
+}
+db.init_app(app)
+db.connection.drop_database(db_name)
+
+# Init login manager
+lm = LoginManager()
+lm.init_app(app)
+
+# Models
+class User(db.Document, UserMixin):
+    name = db.StringField(unique=True, required=True)
+    email = db.StringField(unique=True)
+    api_key = db.StringField(unique=True)
+
+    def get_id(self):
+        return unicode(self.id)
+
+# some related to LoginManager
+@lm.user_loader
+def load_user(userid):
+    print "load_user called"
+    user = User.objects(id=userid).first()
+    if user is None:
+        return None
+    return user
+
+@lm.request_loader
+def load_user_from_request(request):
+    # first, try to login using the api_key url arg
+    api_key = request.args.get('api_key')
+    if api_key is not None:
+        print "api_key: {}".format(api_key)
+        user = User.objects(api_key=api_key).first()
+        if user:
+            return user
+
+    return None
+
+#@lm.unauthorized_handler
+#def unauthorized():
+#    # do stuff
+#    data = {"result": "You need login"}
+#    return jsonify(data)
+
+# Controllers
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/hello_protected")
+@login_required
+def hello_protected():
+    """Can be called as the following.
+    $ curl "http://localhost:5000/hello_protected?api_key=..."
+    """
+    print "hello_protected called"
+    return "Hello World!"
+
+@app.route("/add", methods=["POST"])
+def add():
+    """Called as the following.
+    $ curl -H "Content-Type: application/json" -X POST -d '{"a":"10","b":"20"}' \
+    http://localhost:5000/add
+    """
+
+    data = request.json
+    ret = int(data["a"]) + int(data["b"])
+    res = {"result": ret}
+    return jsonify(res)
+
+if __name__ == "__main__":
+    # Create protected user
+    api_key = "03094200e64fba27239b554d879d9ca654"
+    user = User(name="me", api_key=api_key)
+    user.save()
+    app.run()
