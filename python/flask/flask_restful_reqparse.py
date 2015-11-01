28119641ac0175b5100d84f9ee4678e58be2a67f
kzky <rkzfilter@gmail.com>
2015-11-02 01:08:54 +0900

    add flask restfull reqparse sample.

--- python/flask/flask_restful_reqparse.py
+++ python/flask/flask_restful_reqparse.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
@@ -6,11 +6,12 @@ api = Api(app)
 
 # Hello World
 class HelloWorld(Resource):
-    
+
     parser = reqparse.RequestParser()
     parser.add_argument('rate', type=int, help='Rate cannot be converted')
-    parser.add_argument('name')
+    parser.add_argument('name', required=True)
     parser.add_argument('name_relocated', dest="name_dest")
+    parser.add_argument('todos', type=str, action="append")
         
     def post(self):
         args = self.parser.parse_args()
@@ -20,3 +21,5 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/hello')
 
+if __name__ == '__main__':
+    app.run(debug=True)
