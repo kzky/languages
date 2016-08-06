af697799c7300530eba0ea3667257ad037c4e21e
kzky <rkzfilter@gmail.com>
2016-08-06 22:56:27 +0900

    Add cdef for return val.

--- cython/speedup_exmaple/main.py
+++ cython/speedup_exmaple/main.py
@@ -3,6 +3,7 @@ import integrate_py
 import pyximport; pyximport.install(pyimport = True)
 import integrate_cython
 import integrate_cdef_cython
+import integrate_cdef_v2_cython
 
 def main():
     a = -100.
@@ -24,10 +25,16 @@ def main():
     et_cdef_cython = time.time() - st
     print("cdef Cython: {} [s]".format(et_cdef_cython))
 
+    st = time.time()
+    print(integrate_cdef_v2_cython.integrate_f(a, b, N))
+    et_cdef_v2_cython = time.time() - st
+    print("cdef Cython: {} [s]".format(et_cdef_v2_cython))
+
     print("\n### Ratio Comparison### ")
     print("Python: {} [s]".format(et_python/et_python))
     print("Cython: {} [s]".format(et_python/et_cython))
     print("cdef Cython: {} [s]".format(et_python/et_cdef_cython))
+    print("cdef v2 Cython: {} [s]".format(et_python/et_cdef_v2_cython))
     
 
 if __name__ == "__main__":
