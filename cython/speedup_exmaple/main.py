a168f66e3c676ff3b63eb12c8bd02f7c80719ac8
kzky <rkzfilter@gmail.com>
2016-08-06 22:44:33 +0900

    Add speedup example.

---
+++ cython/speedup_exmaple/main.py
@@ -0,0 +1,35 @@
+import time
+import integrate_py
+import pyximport; pyximport.install(pyimport = True)
+import integrate_cython
+import integrate_cdef_cython
+
+def main():
+    a = -100.
+    b = 100.
+    N = 1000000
+
+    st = time.time()
+    print(integrate_py.integrate_f(a, b, N))
+    et_python = time.time() - st
+    print("Python: {} [s]".format(et_python))
+
+    st = time.time()
+    print(integrate_cython.integrate_f(a, b, N))
+    et_cython = time.time() - st
+    print("Cython: {} [s]".format(et_cython))
+
+    st = time.time()
+    print(integrate_cdef_cython.integrate_f(a, b, N))
+    et_cdef_cython = time.time() - st
+    print("cdef Cython: {} [s]".format(et_cdef_cython))
+
+    print("\n### Ratio Comparison### ")
+    print("Python: {} [s]".format(et_python/et_python))
+    print("Cython: {} [s]".format(et_python/et_cython))
+    print("cdef Cython: {} [s]".format(et_python/et_cdef_cython))
+    
+
+if __name__ == "__main__":
+    main()
+
