a60da44ad11cfdc919e21312bbb3d388696ee7c7
kzky <rkzfilter@gmail.com>
2016-08-12 01:11:16 +0900

    ADD more.

--- cython/tutorial/numpy/main.py
+++ cython/tutorial/numpy/main.py
@@ -1,3 +1,16 @@
+"""
+Result typed input and styped slice numpy is much faster when N=100
+
+Intel(R) Core(TM) i5-2540M CPU @ 2.60GHz
+
+# Ratio Comparison
+python: 1.0
+cython: 0.868703724676
+cython (typed): 1.22492679267
+cython (slice typed): 135.697795453
+cython (slice typed and more): 6875.01741294
+"""
+
 import pyximport; pyximport.install()
 import convolution_py
 import convolution_pyx
@@ -8,38 +21,38 @@ import numpy as np
 import time
 
 def main():
+    # Image and filter
+    N = 1000
+    f = np.arange(N*N, dtype=np.int).reshape((N,N))
+    g = np.arange(81, dtype=np.int).reshape((9, 9))
+    
     # Python
     st = time.time()
-    convolution_py.naive_convolve(np.array([[1, 1, 1]], dtype=np.int),
-                               np.array([[1],[2],[1]], dtype=np.int))
+    convolution_py.naive_convolve(f, g)
     et_py = time.time() - st
     print("python: {} [s]".format(et_py))
 
     # Cython
     st = time.time()
-    convolution_pyx.naive_convolve(np.array([[1, 1, 1]], dtype=np.int),
-                            np.array([[1],[2],[1]], dtype=np.int))
+    convolution_pyx.naive_convolve(f, g)
     et_pyx = time.time() - st
     print("cpython: {} [s]".format(et_pyx))
 
     # Cython (typed)
     st = time.time()
-    convolution_pyx_v2.naive_convolve(np.array([[1, 1, 1]], dtype=np.int),
-                            np.array([[1],[2],[1]], dtype=np.int))
+    convolution_pyx_v2.naive_convolve(f, g)
     et_pyx_v2 = time.time() - st
     print("cpython (typed): {} [s]".format(et_pyx_v2))
 
     # Cython (slice typed)
     st = time.time()
-    convolution_pyx_v3.naive_convolve(np.array([[1, 1, 1]], dtype=np.int),
-                            np.array([[1],[2],[1]], dtype=np.int))
+    convolution_pyx_v3.naive_convolve(f, g)
     et_pyx_v3 = time.time() - st
     print("cpython (slice typed): {} [s]".format(et_pyx_v3))
 
     # Cython (slice typed and more)
     st = time.time()
-    convolution_pyx_v4.naive_convolve(np.array([[1, 1, 1]], dtype=np.int),
-                            np.array([[1],[2],[1]], dtype=np.int))
+    convolution_pyx_v4.naive_convolve(f, g)
     et_pyx_v4 = time.time() - st
     print("cpython (slice typed and more): {} [s]".format(et_pyx_v4))
 
