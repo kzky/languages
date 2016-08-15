cd3ed91515e953c7d6fe878f70e054a4485a28f1
kzky <rkzfilter@gmail.com>
2016-08-16 00:55:42 +0900

    Correct.

--- python/tensorflow/cnn/sandbox/batch_norm.py
+++ python/tensorflow/cnn/sandbox/batch_norm.py
@@ -4,17 +4,35 @@ import numpy as np
 def main():
 
     # Afifne
-    x = np.random.rand(64, 128)
+    x = np.random.rand(64, 128).astype(np.float32)
     mu, var = tf.nn.moments(x, axes=[0])
-    print "mu", mu.get_shape()
-    print "var", var.get_shape()
+    print("mu", mu.get_shape())
+    print("var", var.get_shape())
+
+    gamma = tf.Variable(tf.truncated_normal(shape=[128]))
+    beta = tf.Variable(tf.truncated_normal(shape=[128]))
+    bn = tf.nn.batch_normalization(x, mu, var, beta, gamma,
+                                       variance_epsilon=1e-12)
+
+    init_op = tf.initialize_all_variables()
+    with tf.Session() as sess:
+        sess.run(init_op)
+        print(sess.run(bn))
 
     # Convolution
-    x = np.random.rand(64, 32, 32, 128)
+    x = np.random.rand(64, 32, 32, 128).astype(np.float32)
     mu, var = tf.nn.moments(x, axes=[0, 1, 2])
-    print "mu", mu.get_shape()
-    print "var", var.get_shape()
+    print("mu", mu.get_shape())
+    print("var", var.get_shape())
+    gamma = tf.Variable(tf.truncated_normal(shape=[128]))
+    beta = tf.Variable(tf.truncated_normal(shape=[128]))
+    bn = tf.nn.batch_normalization(x, mu, var, beta, gamma,
+                                       variance_epsilon=1e-12)
     
+    init_op = tf.initialize_all_variables()
+    with tf.Session() as sess:
+        sess.run(init_op)
+        print(sess.run(bn))
 
 if __name__ == '__main__':
     main()
