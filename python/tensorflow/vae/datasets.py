dd7cd2581a2ff687c68bc7fe93cf8e9266366902
kzky <rkzfilter@gmail.com>
2016-08-15 17:42:43 +0900

    Use binarized input.

--- python/tensorflow/vae/datasets.py
+++ python/tensorflow/vae/datasets.py
@@ -6,6 +6,11 @@ class DataReader(object):
             train_path="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/train.npz", 
             test_path="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/test.npz",
             batch_size=32):
+        """
+        Args:
+          train_path: Dict, NpzFile, or some like that, one key-value holds whole data.
+          test_path: Dict, NpzFile, or some like that, one key-value holds whole data.
+        """
             
         self.train_data = np.load(train_path)
         self.test_data = np.load(test_path)
@@ -18,11 +23,18 @@ class DataReader(object):
         self._n_test_data = len(self.test_data["x"])
 
     def get_train_batch(self,):
+        """Return next batch data.
+
+        Returns:
+           tuple of 2: First is for sample and the second is for label.
+                               First data is binarized if a value is greater than 0, then 1;
+                               otherwise 0.
+        """
         # Read data
-        batch_data_x = \
-          self.train_data["x"][self._next_position_train:self._next_position_train+self._batch_size]
-        batch_data_y = \
-          self.train_data["y"][self._next_position_train:self._next_position_train+self._batch_size]
+        beg = self._next_position_train
+        end = self._next_position_train+self._batch_size
+        batch_data_x = np.where(self.train_data["x"][beg:end, :]>0, 1, 0)
+        batch_data_y = self.train_data["y"][beg:end, :]
 
         # Reset pointer
         self._next_position_train += self._batch_size
@@ -32,11 +44,20 @@ class DataReader(object):
         return batch_data_x, batch_data_y
             
     def get_test_batch(self,):
+        """Return next batch data.
+
+        Returns:
+           tuple of 2: First is for sample and the second is for label.
+                               First data is binarized if a value is greater than 0, then 1;
+                               otherwise 0.
+        """
+
         # Read data
-        batch_data_x = \
-          self.test_data["x"][self._next_position_test:self._next_position_test+self._batch_size]
-        batch_data_y = \
-          self.test_data["y"][self._next_position_test:self._next_position_test+self._batch_size]
+        beg = self._next_position_test
+        end = self._next_position_test+self._batch_size
+
+        batch_data_x = np.where(self.test_data["x"][beg:end, :]>0, 1, 0)
+        batch_data_y = self.test_data["y"][beg:end, :]
 
         # Reset pointer
         self._next_position_test += self._batch_size
@@ -45,4 +66,3 @@ class DataReader(object):
 
         return batch_data_x, batch_data_y
 
-    
