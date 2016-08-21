34eeb01bb943c5fc14cd4e046b46ea545e59be76
kzky <rkzfilter@gmail.com>
2016-08-21 22:07:33 +0900

    Fix duplicate sigmoid.

--- python/tensorflow/vae/model.py
+++ python/tensorflow/vae/model.py
@@ -86,7 +86,6 @@ class VAE(object):
         dec_scope = tf.variable_scope("decoder")
         h1 = self._MLP(h0, self._mid_dim, self._in_dim, dec_scope)
 
-        #TODO: duplicate sigmoi
         y = tf.nn.sigmoid(h1)
 
         self.decode = y
@@ -104,11 +103,11 @@ class VAE(object):
         # Decoder loss
         x = self._x
         y = self.decode
-        #binary_cross_entropy = \  # this code will be overflow
-        #  tf.reduce_sum(x * tf.log(y) + (1 - x) * tf.log(1 - y), reduction_indices=[1])
+        binary_cross_entropy = tf.reduce_sum(x * tf.log(y) + (1 - x) * tf.log(1 - y),
+                                            reduction_indices=[1]) # this code will overflow
 
         # Note tf.nn.sigmoid_cross_entropy_with_logits returns `minus`
-        binary_cross_entropy = - tf.nn.sigmoid_cross_entropy_with_logits(y, x)
+        #binary_cross_entropy = - tf.nn.sigmoid_cross_entropy_with_logits(y, x)
         decoder_loss = tf.reduce_mean(binary_cross_entropy)
         
         self.obj = encoder_loss + decoder_loss
