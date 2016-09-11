c1461172390b186f88031af0b3970b1a62d06ce9
kzky <rkzfilter@gmail.com>
2016-09-11 19:29:38 +0900

    [WIP] impl data reader.

--- python/tensorflow/ssl_ladder/datasets.py
+++ python/tensorflow/ssl_ladder/datasets.py
@@ -1,28 +1,35 @@
 import numpy as np
+import os
 
 class DataReader(object):
 
     def __init__(self,
-            train_path="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/train.npz", 
-            test_path="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/test.npz",
-            batch_size=32):
+        l_train_path=\
+            "/home/kzk/.chainer/dataset/pfnet/chainer/mnist/l_train.npz",
+        u_train_path=\
+            "/home/kzk/.chainer/dataset/pfnet/chainer/mnist/u_train.npz", 
+        test_path="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/test.npz",
+        batch_size=32):
         """
         Args:
           train_path: Dict, NpzFile, or some like that, one key-value holds whole data.
           test_path: Dict, NpzFile, or some like that, one key-value holds whole data.
         """
             
-        self.train_data = np.load(train_path)
+        self.l_train_data = np.load(l_train_path)
+        self.u_train_data = np.load(u_train_path)
         self.test_data = np.load(test_path)
 
         self._batch_size = batch_size
-        self._next_position_train = 0
+        self._next_position_l_train = 0
+        self._next_position_u_train = 0
         self._next_position_test = 0
 
-        self._n_train_data = len(self.train_data["x"])
+        self._n_l_train_data = len(self.l_train_data["x"])
+        self._n_u_train_data = len(self.u_train_data["x"])
         self._n_test_data = len(self.test_data["x"])
 
-    def get_train_batch(self,):
+    def get_l_train_batch(self,):
         """Return next batch data.
 
         Returns:
@@ -31,18 +38,59 @@ class DataReader(object):
                                otherwise 0.
         """
         # Read data
-        beg = self._next_position_train
-        end = self._next_position_train+self._batch_size
-        batch_data_x = np.where(self.train_data["x"][beg:end, :]>0, 1., 0.)
-        batch_data_y = self.train_data["y"][beg:end]
+        beg = self._next_position_l_train
+        end = self._next_position_l_train+self._batch_size
+        batch_data_x_ = self.l_train_data["x"][beg:end, :]
+        batch_data_y_ = self.l_train_data["y"][beg:end]
+
+        # Change to one-hot representaion
+        batch_data_y = np.zeros((len(batch_data_y_), self._n_cls))
+        batch_data_y[np.arange(len(batch_data_y_)), batch_data_y_] = 1
 
         # Reset pointer
-        self._next_position_train += self._batch_size
-        if self._next_position_train >= self._n_train_data:
-            self._next_position_train = 0
+        self._next_position_l_train += self._batch_size
+        if self._next_position_l_train >= self._n_l_train_data:
+            self._next_position_l_train = 0
+
+            # shuffle
+            idx = np.arange(self._n_l_train_data)
+            np.random.shuffle(idx)
+            self.l_train_data["x"] = self.l_train_data["x"][idx]
+            self.l_train_data["y"] = self.l_train_data["y"][idx]
         
         return batch_data_x, batch_data_y
-            
+
+    def get_u_train_batch(self,):
+        """Return next batch data.
+
+        Returns:
+           tuple of 2: First is for sample and the second is for label.
+                               First data is binarized if a value is greater than 0, then 1;
+                               otherwise 0.
+        """
+        # Read data
+        beg = self._next_position_u_train
+        end = self._next_position_u_train+self._batch_size
+        batch_data_x_ = self.u_train_data["x"][beg:end, :]
+        batch_data_y_ = self.u_train_data["y"][beg:end]
+
+        # Change to one-hot representaion
+        batch_data_y = np.zeros((len(batch_data_y_), self._n_cls))
+        batch_data_y[np.arange(len(batch_data_y_)), batch_data_y_] = 1
+
+        # Reset pointer
+        self._next_position_u_train += self._batch_size
+        if self._next_position_u_train >= self._n_u_train_data:
+            self._next_position_u_train = 0
+
+            # shuffle
+            idx = np.arange(self._n_u_train_data)
+            np.random.shuffle(idx)
+            self.u_train_data["x"] = self.u_train_data["x"][idx]
+            self.u_train_data["y"] = self.u_train_data["y"][idx]
+        
+        return batch_data_x, batch_data_y
+
     def get_test_batch(self,):
         """Return next batch data.
 
@@ -56,8 +104,13 @@ class DataReader(object):
         beg = self._next_position_test
         end = self._next_position_test+self._batch_size
 
-        batch_data_x = np.where(self.test_data["x"][beg:end, :]>0, 1, 0)
-        batch_data_y = self.test_data["y"][beg:end]
+        batch_data_x_ = self.test_data["x"][beg:end, :]
+        batch_data_x = np.reshape(batch_data_x_, (len(batch_data_x_), 28, 28, 1))
+        
+        # Change to one-hot representaion
+        batch_data_y_ = self.test_data["y"][beg:end]
+        batch_data_y = np.zeros((len(batch_data_y_), self._n_cls))
+        batch_data_y[np.arange(len(batch_data_y_)), batch_data_y_] = 1
 
         # Reset pointer
         self._next_position_test += self._batch_size
@@ -66,3 +119,42 @@ class DataReader(object):
 
         return batch_data_x, batch_data_y
 
+class Separator(object):
+    """Seprate the original samples to labeled samples and unlabeled samples.
+
+    Seprate the original samples to labeled samples and unlabeled samples in such
+    way; the number of labeled samples are selected randomly, it is equal to `l`, 
+    and the others are unlabeled samples.
+    """
+
+    def __init__(self, u=100):
+        self.u = u
+
+    def separate_then_save(self,
+                fpath="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/train.npz"):
+        ldata, udata = self._seprate(fpath)
+        self._save_ssl_data(fpath, ldata, udata)
+        
+    def _separate(self,
+                 fpath="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/train.npz"):
+        
+        data = np.load(fpath)
+        n = len(data)
+        idxs = np.arange(n)
+        idxs_l = np.random.choice(idxs, size=self.u, replace=False)
+        idxs_u = np.asarray(list(set(idxs) - set(idx_l)))
+        
+        return data[idxs_l], data[idxs_u]
+        
+    def _save_ssl_data(self, fpath, ldata, udata):
+        dpath = os.path.dirname(fpath)
+        fname = os.path.basename(fpath)
+
+        l_fname = "l_{}".format(fname)
+        u_fname = "u_{}".format(fname)
+        
+        ldata_fpath = os.path.join(dpath, l_frame)
+        udata_fpath = os.path.join(dpath, u_frame)
+
+        np.savez(ldata_fpath)
+        np.savez(udata_fpath)
--- python/tensorflow/ssl_ladder/main.py
+++ python/tensorflow/ssl_ladder/main.py
@@ -1,5 +1,5 @@
 from models import SSLLadder
-from datasets import DataReader
+from datasets import DataReader, Seprator
 import tensorflow as tf
 import numpy as np
 import time
@@ -8,26 +8,39 @@ import __future__
 
 def main():
     # Setting
+    u = 100
     n_train_data = 60000
+    n_l_train_data = 60000 - u
+    n_u_train_data = u
     batch_size = 128
     in_dim = 784
+    L = 3
     mid_dim = n_dim = 100
     out_dim = n_cls = 10
     n_epoch = 30
-    n_iter = 30 * n_train_data / batch_size
+    n_iter = 30 * n_u_train_data / batch_size
+
+    # Separate
+    home = os.environ.get("HOME")
+    fpath = os.path.join(home, ".chainer/dataset/pfnet/chainer/mnist/train.npz")
+    separator = Separator(u)
+    separator.separate_then_save(fpath)
 
     # Placeholder
     x_l = tf.placeholder(tf.float32, shape=[None, in_dim], name="x")
+    y_l = tf.placeholder(tf.float32, shape=[None, out_dim], name="y")
     x_u = tf.placeholder(tf.float32, shape=[None, in_dim], name="x")
-    y = tf.placeholder(tf.float32, shape=[None, out_dim], name="y")
+
     
     # Model
     ssl_ladder = SSLLadder(x_l, y, x_u, L, n_dim, n_cls, phase_train)
 
     # Data
     home = os.environ["HOME"]
-    train_path="{}/.chainer/dataset/pfnet/chainer/mnist/train.npz".format(home)
-    test_path="{}//.chainer/dataset/pfnet/chainer/mnist/test.npz".format(home)
+    train_path = \
+        "{}/.chainer/dataset/pfnet/chainer/mnist/l_train.npz".format(home)
+    test_path = \
+        "{}/.chainer/dataset/pfnet/chainer/mnist/u_test.npz".format(home)
 
     data_reader = DataReader(train_path, test_path, batch_size=batch_size)
 
@@ -43,10 +56,11 @@ def main():
         epoch = 0
         for i in range(n_iter):
             # Read
-            x_l, y_l, x_u = data_reader.get_train_batch()
+            x_l_data, y_l_data = data_reader.get_l_train_batch()
+            x_u_data, y_u_data = data_reader.get_u_train_batch()
 
             # Train
-            train_step.run(feed_dict={x: x_l})
+            train_step.run(feed_dict={x_l: x_l_data, y_l: y_l_data, x_u: x_u_data})
              
             # Eval
             if (i+1) % (n_train_l / batch_size) == 0:
@@ -69,7 +83,11 @@ def main():
                         break
 
                     acc, loss = sess.run([ssl_ladder.accuracy, ssl_ladder.loss],
-                                             feed_dict={x: x_l, y: y_l, phase_train: False})
+                                         feed_dict={
+                                             x_l: x_l_data,
+                                             y_l: y_l_data,
+                                             x_u: x_u_data,
+                                             phase_train: False})
                     accuracies.append(acc)
                     losses.append(loss)
                     data_points.append(len(y_l))
