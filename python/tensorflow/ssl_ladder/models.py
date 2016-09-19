79f3d010416e18c7d13701a63b8210c46e366ce7
kzky <rkzfilter@gmail.com>
2016-09-19 20:07:26 +0900

    Add bias.

--- python/tensorflow/ssl_ladder/models.py
+++ python/tensorflow/ssl_ladder/models.py
@@ -103,7 +103,7 @@ class SSLLadder(object):
 
         with variable_scope:
             W = tf.get_variable(name=w_name, shape=ksize)
-            #b = tf.get_variable(name=b_name, shape=[ksize[-1]])
+            b = tf.get_variable(name=b_name, shape=[ksize[-1]])
               
         conv2d_op = tf.nn.conv2d(x, W, strides=strides, padding=padding)
         return conv2d_op
@@ -142,7 +142,7 @@ class SSLLadder(object):
 
         with variable_scope:
             W = tf.get_variable(name=w_name, shape=[in_dim, out_dim])
-            #b = tf.get_variable(name=b_name, shape=[out_dim])
+            b = tf.get_variable(name=b_name, shape=[out_dim])
             
         x_ = tf.reshape(x, [-1, in_dim])
         linear_op = tf.matmul(x_, W)
