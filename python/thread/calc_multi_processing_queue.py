effdc7a7b3b78bbfa968dc4f41f79310313be237
kzky <rkzfilter@gmail.com>
2016-10-17 23:52:31 +0900

    Add calc of passing between pipes.

---
+++ python/thread/calc_multi_processing_queue.py
@@ -0,0 +1,34 @@
+from multiprocessing import Process, Queue
+from multiprocessing.queues import SimpleQueue
+import numpy as np
+import time
+
+def f(q_inp, q_out):
+    data = q_inp.get()
+    st = data["st"]
+    et = time.time()
+    print("Chilld:{}[s]".format(et - st))
+    st = time.time()
+    data["st"] = st
+    q_out.put(data)
+
+if __name__ == '__main__':
+    data = {
+        "x": np.random.rand(128, 3, 224, 224),
+        "y": np.random.rand(128, 1000),
+    }
+    q_inp = Queue()
+    q_out = Queue()
+
+    st = time.time()
+    data["st"] = st
+    q_inp.put(data)
+    p = Process(target=f, args=(q_inp, q_out))
+    p.start()
+    data = q_out.get()
+    p.join()
+    st = data["st"]
+    et = time.time()
+    print("Parent:{}[s]".format(et - st))
+    
+    p.terminate()
