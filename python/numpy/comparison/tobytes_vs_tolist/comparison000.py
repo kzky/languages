636bb39eb7ea219a0538273f87dafa94052a6c67
kzky <rkzfilter@gmail.com>
2017-03-31 02:05:43 +0900

    Add sampels.

---
+++ python/numpy/comparison/tobytes_vs_tolist/comparison000.py
@@ -0,0 +1,34 @@
+import numpy as np
+import time
+import marshal
+
+def main():
+
+    num_layers = 10
+    num_param = int(1* 1e6)
+    params = []
+
+    for i in range(num_layers):
+        params.append(np.random.rand(num_param) * 10).astype(np.int32)
+
+    # tobytes and join
+    st = time.time()
+    data = []
+    for param in params:
+        data.append(param.tobytes())
+    "".join(data)
+    et = time.time() - st
+    print("ElapsedTime:{}[s]".format(et))    
+    
+    # tolist and marshal
+    st = time.time()
+    data = []
+    for param in params:
+        data.append(param.tolist())
+    marshal.dumsp(data)
+    et = time.time() - st
+    print("ElapsedTime:{}[s]".format(et))
+
+    
+if __name__ == '__main__':
+    main()
