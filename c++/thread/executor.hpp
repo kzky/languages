b9a451f75880130d34cd6cadbd7e229d910e7207
KazukiYoshiyama <Kazuki.Yoshiyama@jp.sony.com>
2018-01-10 01:13:32 +0900

    Change interface.

--- c++/thread/executor.hpp
+++ c++/thread/executor.hpp
@@ -1,11 +1,16 @@
+/*
+	Minimal Examples of ExecutorPool
+ */
+
 #include <mutex>
 #include <condition_variable>
 #include <queue>
 #include <future>
 #include <thread>
+#include <utility>
+#include <memory>
 
-
-template<typename T>
+template<typename T, typename R>
 class BlockingQueue {
 private:
 	std::mutex mutex_;
@@ -15,18 +20,20 @@ private:
 public:
 	BlockingQueue();
 	~BlockingQueue();
-	void push(T const &task);
-	T pop();
+	void push(std::pair<T, std::promise<R>> &&item);
+	std::pair<T, std::promise<R>> pop();
 };
 
-template<typename T>
+template<typename T, typename R>
 class ExecutorPool {
 private:
-	BlockingQueue<T> queue_;
 	int pool_size_;
+	BlockingQueue<T, R> queue_;
+	std::vector<std::thread> thread_pool_;
+	bool is_shutdown_;
 public:
 	ExecutorPool(int pool_size);
 	~ExecutorPool();
-	std::future<T> submit(T task);
+	std::future<R> submit(T const &task);
 	void shutdown();
 };
